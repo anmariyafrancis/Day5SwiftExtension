a7d8737a9921140acda96da6f64eb867ec575a3a
anmariyafrancis <anmariya1915@gmail.com>
2020-02-12 19:26:27 -0500

    for loop in extension

--- Day5SwiftExtension/main.swift
+++ Day5SwiftExtension/main.swift
@@ -15,11 +15,28 @@ extension Double
     var cm:Double{return self/100.0}
     var mm:Double{return self/1000.0}
     var ft:Double{return self/3.28084}
+    
+    func currency()->String
+    {
+        let s=String.init(format:"$%.2f",self)
+        return s
+    }
 }
 var meter=100.0
 print(meter.km)
 print(meter.m)
+print(meter.currency())
 
+extension String
+{
+    func repeatNTimes(n:Int)
+    {
+        for _ in 1...n
+        {
+            print(self)
+        }
+    }
+    
+}
 
-
-
+"HELLO".repeatNTimes(n: 10)
